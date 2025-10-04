4822070eaaf22996ed50fd8e62a0350fee26d4db
Jan Gruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-10-04 20:52:40 +0200

    add configs

--- conf/gnn4/t1.py
+++ conf/gnn4/t1.py
@@ -1,21 +1,28 @@
 from hrl.common.agent import AgentConfig
+from hrl.common.reward import EvalConfig
+from hrl.common.storage import StorageConfig
 from hrl.experiments.pepr import PePrConfig
 from hrl.master_train import TrainConfig
 from hrl.networks import NetworkType
-from hrl.env.calvin import MasterEnvConfig
+from hrl.env.environment import EnvironmentConfig
 
+storage = StorageConfig(
+    skills_tag="Minimal",
+    states_tag="Minimal",
+)
 config = TrainConfig(
     tag="t1",
+    nt=NetworkType.GNN_V4,
     experiment=PePrConfig(
-        states_tag="Minimal",
-        skills_tag="Minimal",
-        p_empty=0.0,
+        p_empty=0.8,
         p_rand=0.0,
-        env=MasterEnvConfig(
-            debug_vis=False,
-        ),
-        agent=AgentConfig(),
-        nt=NetworkType.GNN_V4,
     ),
-    use_wandb=False,
+    env=EnvironmentConfig(render=False),
+    agent=AgentConfig(),
+    eval=EvalConfig(
+        step_reward=-1.0,
+        success_reward=100.0,
+    ),
+    storage=storage,
+    use_wandb=True,
 )
--- conf/gnn4/z2.py
+++ conf/gnn4/z2.py
@@ -1,19 +1,28 @@
 from hrl.common.agent import AgentConfig
+from hrl.common.reward import EvalConfig
+from hrl.common.storage import StorageConfig
 from hrl.experiments.pepr import PePrConfig
 from hrl.master_train import TrainConfig
 from hrl.networks import NetworkType
-from hrl.env.calvin import MasterEnvConfig
+from hrl.env.environment import EnvironmentConfig
 
+storage = StorageConfig(
+    skills_tag="Normal",
+    states_tag="Normal",
+)
 config = TrainConfig(
     tag="z2",
+    nt=NetworkType.GNN_V4,
     experiment=PePrConfig(
-        states_tag="Normal",
-        skills_tag="Normal",
         p_empty=0.0,
         p_rand=0.0,
-        env=MasterEnvConfig(min_reward=0.0),
-        agent=AgentConfig(),
-        nt=NetworkType.GNN_V4,
     ),
+    env=EnvironmentConfig(render=False),
+    agent=AgentConfig(),
+    eval=EvalConfig(
+        step_reward=0.0,
+        success_reward=100.0,
+    ),
+    storage=storage,
     use_wandb=True,
 )
