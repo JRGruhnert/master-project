6a47172e09d74e96b02e0d61330c0e04bdc51d87
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-11-20 03:12:52 +0100

    Update buffer.py

--- src/modules/buffer.py
+++ src/modules/buffer.py
@@ -96,34 +96,9 @@ class Buffer:
 
     def metrics(self) -> dict[str, float]:
         assert self.health(), "Buffer lengths are inconsistent!"
-        # Calculate episode statistics
-        episode_rewards = []
-        episode_lengths_batch = []
-        episode_success = []
-
-        current_episode_reward = 0
-        current_episode_length = 0
-
-        for i, reward in enumerate(self.rewards):
-            current_episode_reward += reward
-            current_episode_length += 1
-            current_episode_success = int(self.success[i])
-
-            if i == len(self.rewards) - 1 or self.terminals[i]:
-                episode_rewards.append(current_episode_reward)
-                episode_lengths_batch.append(current_episode_length)
-                episode_success.append(current_episode_success)
-                current_episode_reward = 0
-                current_episode_length = 0
-
         return {
-            "total_reward": sum(episode_rewards),
-            "average_length": (
-                sum(episode_lengths_batch) / len(episode_lengths_batch)
-                if episode_lengths_batch
-                else 0
-            ),
-            "success_rate": (
-                sum(episode_success) / len(episode_success) if episode_success else 0
-            ),
+            "total_reward": sum(self.rewards),
+            "average_reward": sum(self.rewards) / self.config.batch_size,
+            "average_length": self.terminals.count(True) / self.config.batch_size,
+            "success_rate": self.success.count(True) / self.terminals.count(True),
         }
