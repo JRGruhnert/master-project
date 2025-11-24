dd278d5f97ba8b21a8201c4026ec556e69a2f56e
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-11-24 19:16:52 +0100

    add more tiers

---
+++ conf/baseline/ts_bb.py
@@ -0,0 +1,38 @@
+from src.agents.ppo.baseline import BaselineAgentConfig
+from src.environments.calvin import CalvinEnvironmentConfig
+from src.modules.buffer import BufferConfig
+from src.modules.evaluators.sparse import SparseEvaluatorConfig
+from src.modules.logger import LogMode, LoggerConfig
+from src.modules.storage import StorageConfig
+from src.experiments.pepr import PePrConfig
+from scripts.train import TrainConfig
+
+mode = LogMode.WANDB
+render = False
+eval = False
+tag = "ts_bb"
+
+config = TrainConfig(
+    agent=BaselineAgentConfig(eval=eval),
+    buffer=BufferConfig(),
+    logger=LoggerConfig(
+        mode=mode,
+        wandb_tag=tag,
+    ),
+    storage=StorageConfig(
+        used_skills="BaseBlue",
+        used_states="BaseRedPinkBlue",
+        eval_states="BaseBlue",
+        tag=tag,
+        network="baseline",
+    ),
+    experiment=PePrConfig(
+        p_empty=0.0,
+        p_rand=0.0,
+    ),
+    environment=CalvinEnvironmentConfig(render=render),
+    evaluator=SparseEvaluatorConfig(
+        step_reward=-0.01,
+        success_reward=1.0,
+    ),
+)
