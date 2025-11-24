304d492d80af37d9f479d6b08a934c8d8d78d195
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-11-24 17:58:57 +0100

    new skill tiers

---
+++ conf/baseline/ts_b.py
@@ -0,0 +1,37 @@
+from src.agents.ppo.baseline import BaselineAgentConfig
+from src.environments.calvin import CalvinEnvironmentConfig
+from src.modules.buffer import BufferConfig
+from src.modules.evaluators.sparse import SparseEvaluatorConfig
+from src.modules.logger import LogMode, LoggerConfig
+from src.modules.storage import StorageConfig
+from src.experiments.pepr import PePrConfig
+from scripts.train import TrainConfig
+
+mode = LogMode.WANDB
+render = False
+eval = False
+tag = "t1_s"
+
+config = TrainConfig(
+    agent=BaselineAgentConfig(eval=eval),
+    buffer=BufferConfig(),
+    logger=LoggerConfig(
+        mode=mode,
+        wandb_tag=tag,
+    ),
+    storage=StorageConfig(
+        used_skills="Minimal",
+        used_states="Minimal",
+        tag=tag,
+        network="baseline",
+    ),
+    experiment=PePrConfig(
+        p_empty=0.0,
+        p_rand=0.0,
+    ),
+    environment=CalvinEnvironmentConfig(render=render),
+    evaluator=SparseEvaluatorConfig(
+        step_reward=-0.01,
+        success_reward=1.0,
+    ),
+)
