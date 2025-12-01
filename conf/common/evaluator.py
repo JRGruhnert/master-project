2ce54ebc112ef0a92c8f9e333bafe4cce432f37f
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-12-01 05:19:38 +0100

    Update evaluator.py

--- conf/common/evaluator.py
+++ conf/common/evaluator.py
@@ -3,12 +3,12 @@ from src.modules.evaluators.sparse import SparseEvaluatorConfig
 
 
 dense_evaluator = Dense2EvaluatorConfig(
-    success_reward=1.0,
-    positive_step_reward=0.1,
-    negative_step_reward=-0.01,
+    success_reward=100.0,
+    positive_step_reward=5.0,
+    negative_step_reward=-1.0,
 )
 
 sparse_evaluator = SparseEvaluatorConfig(
-    step_reward=-0.01,
-    success_reward=1.0,
+    success_reward=100.0,
+    step_reward=-1.0,
 )
