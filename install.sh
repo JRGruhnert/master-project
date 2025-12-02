e108ab1c1c69c857eedc699a53c6e0a6f1748ca1
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-12-02 17:07:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,6 +48,14 @@ else
 fi
 pip install .
 
+
+# Install PyTorch Geometric extensions
+echo "Installing PyTorch Geometric extensions..."
+CUDA_VERSION=$(python -c "import torch; print(torch.version.cuda.replace('.', '')[:3] if torch.version.cuda else 'cpu')")
+TORCH_VERSION="2.1.0"
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f "https://data.pyg.org/whl/torch-${TORCH_VERSION}+cu${CUDA_VERSION}.html"
+
+
 # Install this project (HRL)
 echo "Installing HRL project..."
 cd $HRL_ROOT
