11e99a0521042f0dd595a603409ef08b71dcdd82
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-12-03 20:46:01 +0100

    Update ts_bb.py

--- conf/human/ts_bb.py
+++ conf/human/ts_bb.py
@@ -1,15 +1,15 @@
 from src.agents.human import HumanAgentConfig
 from src.environments.calvin import CalvinEnvironmentConfig
 from src.modules.buffer import BufferConfig
-from src.modules.evaluators.sparse import SparseEvaluatorConfig
 from src.modules.logger import LogMode, LoggerConfig
 from src.modules.storage import StorageConfig
 from src.experiments.pepr import PePrConfig
 from scripts.train import TrainConfig
+from conf.common.evaluator import dense2_evaluator
 
 mode = LogMode.TERMINAL
 render = True
-tag = "ts_bb"
+tag = "human"
 
 config = TrainConfig(
     agent=HumanAgentConfig(),
@@ -30,8 +30,5 @@ config = TrainConfig(
         p_rand=0.0,
     ),
     environment=CalvinEnvironmentConfig(render=render),
-    evaluator=SparseEvaluatorConfig(
-        step_reward=-0.01,
-        success_reward=1.0,
-    ),
+    evaluator=dense2_evaluator,
 )
