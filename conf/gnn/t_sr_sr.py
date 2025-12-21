771a4933ff46e282ab724a77514ffcbd4a6a9996
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-12-21 14:59:22 +0100

    Create t_sr_sr.py

---
+++ conf/gnn/t_sr_sr.py
@@ -0,0 +1,40 @@
+from src.agents.ppo.gnn import GNNAgentConfig
+from src.environments.calvin import CalvinEnvironmentConfig
+from src.modules.buffer import BufferConfig
+from src.modules.logger import LogMode, LoggerConfig
+from src.modules.storage import StorageConfig
+from src.experiments.pepr import PePrConfig
+from scripts.train import TrainConfig
+from conf.common.evaluator import dense3_evaluator
+
+mode = LogMode.WANDB
+render = False
+eval = False
+tag = "t_sr_sr"
+
+config = TrainConfig(
+    agent=GNNAgentConfig(
+        eval=eval,
+        max_batches=300,
+        early_stop_patience=50,
+        min_batches=100,
+    ),
+    buffer=BufferConfig(steps=1024),
+    logger=LoggerConfig(
+        mode=mode,
+        wandb_tag=tag,
+    ),
+    storage=StorageConfig(
+        used_skills="SmallRed",
+        used_states="SmallRed",
+        eval_states="SmallRed",
+        tag=tag,
+        network="gnn",
+    ),
+    experiment=PePrConfig(
+        p_empty=0.0,
+        p_rand=0.0,
+    ),
+    environment=CalvinEnvironmentConfig(render=render),
+    evaluator=dense3_evaluator,
+)
