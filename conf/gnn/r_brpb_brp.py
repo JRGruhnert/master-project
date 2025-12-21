e93c04269e5adff3a1625f75fd443f69ce902273
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2025-12-21 14:59:11 +0100

    Create r_brpb_brp.py

---
+++ conf/gnn/r_brpb_brp.py
@@ -0,0 +1,42 @@
+from src.agents.ppo.gnn import GNNAgentConfig
+from src.environments.calvin import CalvinEnvironmentConfig
+from src.modules.buffer import BufferConfig
+from src.modules.logger import LogMode, LoggerConfig
+from src.modules.storage import StorageConfig
+from src.experiments.pepr import PePrConfig
+from scripts.train import TrainConfig
+from conf.common.evaluator import dense3_evaluator
+
+mode = LogMode.WANDB
+render = False
+eval = False
+tag = "r_brpb_brp"
+
+config = TrainConfig(
+    agent=GNNAgentConfig(
+        eval=eval,
+        max_batches=1000,
+        early_stop_patience=50,
+        min_batches=100,
+        retrain=True,
+    ),
+    buffer=BufferConfig(steps=1024),
+    logger=LoggerConfig(
+        mode=mode,
+        wandb_tag=tag,
+    ),
+    storage=StorageConfig(
+        used_skills="BaseRedPinkBlue",
+        used_states="BaseRedPinkBlue",
+        eval_states="BaseRedPinkBlue",
+        tag=tag,
+        network="gnn",
+        checkpoint_path="results/td3_brpb_br/gnn/best_model.pth",
+    ),
+    experiment=PePrConfig(
+        p_empty=0.0,
+        p_rand=0.0,
+    ),
+    environment=CalvinEnvironmentConfig(render=render),
+    evaluator=dense3_evaluator,
+)
